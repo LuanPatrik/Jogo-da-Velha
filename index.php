1436ba259e8ec6b53f3323932c05de8b425ab7ce
LuanPatrik <72170325+LuanPatrik@users.noreply.github.com>
2023-02-15 00:34:26 -0300

    Adição da tela de jogadores

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/index.css">
+    <title>Jogadores</title>
+</head>
+<body>
+    <div class="titulo">
+        <h1>Digite o nome dos jogadores</h1>
+    </div>
+    <form action="">
+        <div class="jogador">
+            <div class="x">
+                <img src="./image/x.png" alt="">
+                <br>
+                <label for="">Jogador 1:</label>
+                <br>
+                <input type="text">
+                <br>
+            </div>
+            <div class="circulo">
+                <img src="./image/circulo.png" alt="">
+                <br>
+                <label for="">Jogador 2:</label>
+                <br>
+                <input type="text">
+            </div>
+        </div>
+    </form>
+    <div class="botao">
+        <a href="./jogo-velha.php"><button>Jogar</button></a>
+    </div>
+</body>
+</html>
--- jogo-velha.php
+++ jogo-velha.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="./style/jogo-velha.css">
     <title>Jogo da Velha</title>
 </head>
 <body>
+    <div class="voltar">
+        <a href="./index.php"><img src="./image/voltar.png" alt="Voltar"></a>
+    </div>
     <div class="matriz">
         <div class="linha">
             <div class="casa" id="casa1" ></div>
@@ -20,9 +23,9 @@
             <div class="casa" id="casa6"></div>
         </div>
         <div class="linha">
-            <div class="casa" id="casa7"></div>
-            <div class="casa" id="casa8"></div>
-            <div class="casa" id="casa9"></div>
+            <div class="" id="casa7"></div>
+            <div class="" id="casa8"></div>
+            <div class="" id="casa9"></div>
         </div>
     </div>
 </body>
