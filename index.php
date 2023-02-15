9eee69e60b79dd23c2d76508bc0311be7fc7affa
LuanPatrik <72170325+LuanPatrik@users.noreply.github.com>
2023-02-15 16:53:53 -0300

    Update

--- index.php
+++ index.php
@@ -1,39 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
+<DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style/index.css">
-    <title>Jogadores</title>
+    <link rel="stylesheet" type="" href="index.css"/>   
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
 </head>
 <body>
-    <div class="titulo">
-        <h1>Digite o nome dos jogadores</h1>
-    </div>
-    <form action="" method="POST">
-        <div class="jogador">
-            <div class="x">
-                <img src="./image/x.png" alt="">
-                <br>
-                <label for="">Jogador 1:</label>
-                <br>
-                <input type="text" name="nome1">
-                <br>
+        <div id="jogo">
+            <div class="linha">
+                <div class="casa" id="casa1"></div>
+                <div class="casa" id="casa2"></div>
+                <div class="casa" id="casa3"></div>
             </div>
-            <div class="circulo">
-                <img src="./image/circulo.png" alt="">
-                <br>
-                <label for="">Jogador 2:</label>
-                <br>
-                <input type="text">
-            </div
-            >
+            <div class="linha">
+                <div class="casa" id="casa4"></div>
+                <div class="casa" id="casa5"></div>
+                <div class="casa" id="casa6"></div>
+            </div>
+            <div class="linha">
+                <div class="casa" id="casa7"></div>
+                <div class="casa" id="casa8"></div>
+                <div class="casa" id="casa9"></div>
+            </div>
         </div>
-        <div class="botao">
-            <a href="./jogo-velha.php"><button>Jogar</button></a>
-        </div>
-    </form>
-
+        <div id="resultado"></div>
 </body>
 </html>
